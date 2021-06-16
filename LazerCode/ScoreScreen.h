b4ec8669850f104837164f68b4f7d2439f163413
The Game <38539205+jozsef123@users.noreply.github.com>
2021-06-15 18:53:36 -0700

    Update ScoreScreen.h

--- LazerCode/ScoreScreen.h
+++ LazerCode/ScoreScreen.h
@@ -1,4 +1,5 @@
 #include "pch.h"
+//#include "driver.h"
 using namespace sf;
 
 class ScoreScreen
@@ -7,17 +8,24 @@ public:
 	bool ScoreWindow();
 	ScoreScreen();
 	~ScoreScreen();
-	void DrawBackground(RenderTarget &RenderTarget, RectangleShape rectangle);
+	void DrawBackground(RenderTarget &RenderTarget);
+	int* ReadFile(int &count);
 
 private:
 	Event windowEvent;
 	Texture Background;
-	Texture MenuScreen;
-	Sprite SBackground;
-	Sprite SMenuScreen;
+	Texture HighScoreMenuScreen;
+
+	Sprite SpriteBackground;
 	Font font;
 
+	RectangleShape MainMenuButton;
+	RectangleShape MazeGameButton;
+	RectangleShape QuitButton;
+
 	Text MainMenu;
+	Text MazeGame;
+	Text Quit;
 	Text HighScores;
 	Text PlayerScores;
 };
